34ce279621c9ed9ceb6e0d368433395e3fe22c5a
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-03-22 09:02:57 -0300

    feat: configuração dos states

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import { Image, Button, StatusBar, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+
+/* Importando os recursos nativos */
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  /*  State tradicional para armazenar a refêrencia da foto(quando existir) */
+  const [foto, setFoto] = useState(null);
+  /* State de checagem de permissões de uso (através do hook camera permissions) */
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+  console.log(status);
+
   return (
     <>
       <StatusBar />
